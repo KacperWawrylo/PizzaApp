92eda2b7e56a839a1a71cbbf23fed528b2e32ad6
Kacper <kacper.wawrylo@tm1.edu.pl>
2021-11-25 14:07:28 +0100

    v1

---
+++ src/main/java/pl/kacper/pizzaapp/remote/rest/dto/request/AddSizeDto.java
@@ -0,0 +1,4 @@
+package pl.kacper.pizzaapp.remote.rest.dto.request;
+
+public class AddSizeDto {
+}
---
+++ src/main/java/pl/kacper/pizzaapp/remote/rest/dto/response/OrderDto.java
@@ -0,0 +1,4 @@
+package pl.kacper.pizzaapp.remote.rest.dto.response;
+
+public class OrderDto {
+}
